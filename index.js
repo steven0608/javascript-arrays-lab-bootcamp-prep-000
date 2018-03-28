d21d113b5631ae2b38674295b31d4ba15650fc20
Steven Chen <ysteven.chen2@gmail.com>
2018-03-28 03:29:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function prependKitten(name){
   return [name,...kittens];
 }
 function removeLastKitten(){
-  return kittens.slice(0,kittens.length);
+  return kittens.slice(0,kittens.length-1);
 }
