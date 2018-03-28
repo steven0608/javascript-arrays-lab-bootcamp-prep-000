03e7df04535db2bb1c0716a133c03c48337f904c
Steven Chen <ysteven.chen2@gmail.com>
2018-03-28 02:16:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name){
   return kittens.push(name);
 }
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
